c15fc60012897d4f033ab6a0b5a4611afe1788cc
Carlos Álvarez <112678851+cmal15@users.noreply.github.com>
2023-10-01 23:05:06 -0600

    Crear alumno y mod clase alumno

--- Alumno.java
+++ Alumno.java
@@ -6,12 +6,23 @@ public class Alumno{
     private String nombre;
     private float calificacion;
 
-    public Alumno(int numeroDeCuenta, String nombre, float calificacion){
+    public Alumno(int numeroDeCuenta, String nombre){
 
         this.numeroDeCuenta = numeroDeCuenta;
         this.nombre = nombre;
         this.calificacion = 0f;
 
+    }
+
+    public static Alumno nuevo(Scanner sc){
+        int numeroDeCuenta;
+        String nombre;
+        float calificacion;
+        System.out.print("Introduzca numero de cuenta: ");
+        numeroDeCuenta = sc.nextInt();
+        System.out.print("Introduzca nombre: ");
+        nombre = sc.nextLine();
+        return new Alumno(numeroDeCuenta,nombre);
     }
 
     public int getNumeroDeCuenta(){
@@ -32,6 +43,10 @@ public class Alumno{
     public void setCalificacion(float calificacion){
         this.calificacion = calificacion;
     }
-    
+    public void mostrar(){
+        System.out.println("Numero de cuenta: " + this.numeroDeCuenta);
+        System.out.println("Numero de nombre: " + this.nombre);
+        System.out.println("Numero de promedio: " + this.promedio);
+    }
 
 }
--- Principal.java
+++ Principal.java
@@ -12,7 +12,7 @@ public class Principal {
         LinkedList<Grupo> grupos = new LinkedList<Grupo>();
         LinkedList<Asignatura> asignaturas = new LinkedList<Asignatura>();
 
-        int op;
+        int op, op2, aux;
         do {
             Menucito.operaciones();
             op = sc.nextInt();
@@ -20,20 +20,39 @@ public class Principal {
             case 1:
                 //Crear
                 Menucito.elementos();
+                op2 = sc.nextInt();
+                switch(op){
+                    case 1:
+                        alumnos.add(Alumno.nuevo(sc));
+                        break;
+                    case 2:
+                        System.out.println("Mostrando todos los alumnos del sistema");
+                        for(int i = 0; i < alumnos.size(); i++){
+                            System.out.println(alumnos.get(i));
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Introduzca el numero de cuenta del alumno a modificar");
+                        
+                        break;
+                }
                 break;
             case 2:
                 //Consultar
                 Menucito.elementos();
-                
+                op2 = sc.nextInt();
+
                 break;
             case 3:
                 //Modificar
                 Menucito.elementos();
-                
+                op2 = sc.nextInt();
+
                 break;
             case 4:
                 //Eliminar
                 Menucito.elementos();
+                op2 = sc.nextInt();
 
                 break;
            
