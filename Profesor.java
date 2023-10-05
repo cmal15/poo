8f8250cd2ad5a1deb4d95e3cdb9d8d2a1a908edc
Isaias1111 <112528330+Isaias1111@users.noreply.github.com>
2023-10-04 20:49:49 -0600

    Update Profesor.java

--- Profesor.java
+++ Profesor.java
@@ -2,10 +2,12 @@ public class Profesor{
     
     private String nombre;
     private int identificador;
+    private static int numeroProfesor=0;
 
     public Profesor(String nombre, int identificador){
         this.nombre = nombre;
         this.identificador = identificador;
+        numeroProfesor++;
     }
 
     public String getnombre() {
