e85097ee21a1e629df4da95cd2c074d60645f511
Carlos Álvarez <112678851+cmal15@users.noreply.github.com>
2023-10-04 19:14:42 -0600

    Poniendo orden

--- Grupo.java
+++ Grupo.java
@@ -4,20 +4,27 @@ public class Grupo{
     private Profesor profesor;
     private int numero;
     private LinkedList<Alumno> alumnos;
+    private static int cantidad = 0;
     
     public Grupo( Asignatura asignatura,Profesor profesor,int numero,LinkedList<Alumno> alumnos){
         this.asignatura = asignatura;
         this.profesor = profesor;
         this.numero = numero;
         this.alumnos = alumnos;
+        cantidad++;
     }
 
     public Grupo( Asignatura asignatura,Profesor profesor,int numero){
         this.asignatura = asignatura;
         this.profesor = profesor;
         this.numero = numero;
+        cantidad++;
     }
     
+    public static getCantidad(){
+        return cantidad;
+    }
+
     public Asignatura setAsignatura() {
         return asignatura;
     }
--- Principal.java
+++ Principal.java
@@ -27,12 +27,48 @@ public class Principal {
                         alumnos.add(Alumno.nuevo(sc));
                         break;
                     case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        //eliminar
+                }
+                break;
+            case 2:
+                //Consultar
+                Menucito.elementos();
+                op2 = sc.nextInt();
+                switch(op2){
+                    case 1:
+                        //alumno
                         System.out.println("Mostrando todos los alumnos del sistema");
                         for(int i = 0; i < alumnos.size(); i++){
                             System.out.println(alumnos.get(i));
-                        }
+                        }                        
+                        break;
+                    case 2:
+                        //Profesor
                         break;
                     case 3:
+                        //Asignatura
+                        break;
+                    case 4: 
+                        //grupo
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 3:
+                //Modificar
+                Menucito.elementos();
+                op2 = sc.nextInt();
+                switch(op2){
+                    case 1:
+                        //alumno
                         System.out.println("Introduzca el numero de cuenta del alumno a modificar");
                             aux = sc.nextInt();
                             if(alumnos.containsKey(aux)){
@@ -43,27 +79,47 @@ public class Principal {
                                 System.out.println("No hay alumno registrado con ese numero de cuenta");
                             }
                         break;
-                    case 4:
-                        //eliminar
+                    case 2:
+                        //Profesor
+                        break;
+                    case 3:
+                        //Asignatura
+                        break;
+                    case 4: 
+                        //grupo
+                        break;
+                    default:
+                        break;
                 }
-                break;
-            case 2:
-                //Consultar
-                Menucito.elementos();
-                op2 = sc.nextInt();
-
-                break;
-            case 3:
-                //Modificar
-                Menucito.elementos();
-                op2 = sc.nextInt();
-
                 break;
             case 4:
                 //Eliminar
                 Menucito.elementos();
                 op2 = sc.nextInt();
-
+                switch(op2){
+                    case 1:
+                        //alumno
+                        System.out.println("Introduzca el numero de cuenta del alumno a eliminar:")
+                        aux = sc.nextInt();
+                        if(alumnos.containsKey(aux)){
+                            alumnos.remove(aux);
+                            System.out.println("Se elimino el alumno con el numero de cuenta: "+ aux);
+                        }else{
+                            System.out.println("No hay alumno registrado con ese numero de cuenta");
+                        }
+                        break;
+                    case 2:
+                        //Profesor
+                        break;
+                    case 3:
+                        //Asignatura
+                        break;
+                    case 4: 
+                        //grupo
+                        break;
+                    default:
+                        break;
+                }
                 break;
            
             default:
